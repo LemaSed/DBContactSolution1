22a269d9695c97f98c81d6c6f895309dfc187f0b
LemaS <lema_no5166@hotmail.no>
2020-06-30 14:18:21 +0200

    CreateContact added

--- DBContactSolution1/SQLRepository.cs
+++ DBContactSolution1/SQLRepository.cs
@@ -13,14 +13,49 @@ namespace DBContactSolution1
 	public static class SQLRepository
 	{
 		private const string connectionString = @"Server = (local)\MSSQLLocalID; Database = DBscrum; Integrated Security=true";
+		private static readonly SqlConnection Connection;
+		static SQLRepository()  
+		{
+			Connection = new SqlConnection(connectionString);
+		}
 		public static int CreateContact(string ssn, string firstName, string LastName )
 		{
-
+			int identityId = 0;
 			const string cmdText = "INSERT into Contact (SSN, FirstName, LastName)" +
 				"VALUES @ssn, @firstName, @lastName " +
 				"SELECT SCOPE_IDENTITY() as IdentityId ";
 
-		
+			List<SqlParameter> parameterList = new List<SqlParameter> {
+			new SqlParameter("ssn", ssn),
+			new SqlParameter ("firstName",firstName), 
+			new SqlParameter("lastName", LastName)
+			};
+
+			try
+			{
+				using (SqlCommand command = new SqlCommand(cmdText, Connection ))
+				{
+					Connection.Open();
+					using (SqlDataReader reader = command.ExecuteReader())
+					{
+						if (reader.Read())
+						{
+							identityId = (int)(decimal)reader["IdentityID"];
+						}
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				string errMsg = e.Message;
+				return 0;
+			}
+			finally
+			{
+				Connection.Close();
+			}
+
+			return identityId;
 		}
 
 	}
