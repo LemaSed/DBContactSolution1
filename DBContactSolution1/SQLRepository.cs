e861e575f00d11d7ca1a4c2403e88102f90e51ff
LemaS <lema_no5166@hotmail.no>
2020-06-30 13:45:47 +0200

    Work in progress

--- DBContactSolution1/SQLRepository.cs
+++ DBContactSolution1/SQLRepository.cs
@@ -13,15 +13,13 @@ namespace DBContactSolution1
 	public static class SQLRepository
 	{
 		private const string connectionString = @"Server = (local)\MSSQLLocalID; Database = DBscrum; Integrated Security=true";
-		public static int CreateContact()
+		public static int CreateContact(string ssn, string firstName, string LastName )
 		{
-			using (SqlConnection connection = new SqlConnection(connectionString))
-			{
 
-				string queryString = "SELECT contact.Firstname, contact.Lastname, Contact.SSN, Contact.id " +
-				"FROM contact " +
-				"ORDER BY contact.id ASC;";
-			}
+			const string cmdText = "INSERT into Contact (SSN, FirstName, LastName)" +
+				"VALUES @ssn, @firstName, @lastName " +
+				"SELECT SCOPE_IDENTITY() as IdentityId ";
+
 		
 		}
 
