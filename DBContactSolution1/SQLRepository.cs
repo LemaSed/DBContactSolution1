4793ea8cc1b6b6a5d2a0f0967530266bab7ca1f5
LemaS <lema_no5166@hotmail.no>
2020-06-30 10:36:26 +0200

    Ny prosjekt

---
+++ DBContactSolution1/SQLRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace DBContactSolution1
+{
+	class SQLRepository
+	{
+		public static int CreateContact()
+		{
+
+			return
+		}
+
+	}
+}
