ae6f47c8ced40ffd5142c35053f32e5146a2f672
LemaS <lema_no5166@hotmail.no>
2020-06-30 13:32:36 +0200

    Update code

--- DBContactSolution1/SQLRepository.cs
+++ DBContactSolution1/SQLRepository.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Text;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data;
+using System.Data.Common;
+using System.Reflection.Metadata.Ecma335;
 
 namespace DBContactSolution1
 {
-	class SQLRepository
+	public static class SQLRepository
 	{
+		private const string connectionString = @"Server = (local)\MSSQLLocalID; Database = DBscrum; Integrated Security=true";
 		public static int CreateContact()
 		{
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
 
-			return
+				string queryString = "SELECT contact.Firstname, contact.Lastname, Contact.SSN, Contact.id " +
+				"FROM contact " +
+				"ORDER BY contact.id ASC;";
+			}
+		
 		}
 
 	}
